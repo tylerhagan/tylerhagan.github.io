e1655f43738c16a14827f412d2b0c703c92d9b6c
Tyler Hagan <tylerhagan@gmail.com>
2024-01-23 16:26:22 +0100

    More techy flavour/personality

--- assets/switcher.js
+++ assets/switcher.js
@@ -5,11 +5,13 @@ if (!localStorage.getItem('styleSet')) {
 
 function change() {
     if (localStorage.getItem('styleSet') == 'default') {
-    localStorage.setItem("styleSet", 'flat');
-    document.getElementById("styleSwitch").checked = false;
+        localStorage.setItem("styleSet", 'flat');
+        document.getElementById("styleSwitch").checked = false;
+        document.getElementById("logo").src="assets/img/th-logomark-light.svg";
     } else {
-    localStorage.setItem("styleSet", 'default');
-    document.getElementById("styleSwitch").checked = true;
+        localStorage.setItem("styleSet", 'default');
+        document.getElementById("styleSwitch").checked = true;
+        document.getElementById("logo").src="assets/img/th-logomark-dark.svg";
     }
 
     var classStyle = String(localStorage.getItem('styleSet'));
@@ -21,6 +23,8 @@ let root = document.documentElement;
 document.body.className = (classStyle);
 if (localStorage.getItem('styleSet') == 'default') {
     document.getElementById("styleSwitch").checked = true;
+    document.getElementById("logo").src="assets/img/th-logomark-dark.svg";
 } else {
     document.getElementById("styleSwitch").checked = false;
+    document.getElementById("logo").src="assets/img/th-logomark-light.svg";
 }
